e000107dbadc872e66221282d153498fc4b36c97
Joseane <88064526+JosyLeal@users.noreply.github.com>
2025-09-21 17:50:07 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 printf("Teste");
+printf("Novo commit");
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
