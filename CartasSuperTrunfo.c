55acdd284f252a6439036caa2df8f32cf8ab691e
Joseane <88064526+JosyLeal@users.noreply.github.com>
2025-09-21 16:33:01 -0300

    Declaração das variaveis e parte do código de entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,24 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-printf("Teste");
-printf("Novo commit");
+int carta;
+char estado[1]
+char codigo_da_carta [1];
+char[15]string nome_cidade;
+int populacao;
+float pib;
+int numero_de_pontos_turisticos;
+
   // Área para entrada de dados
+printf("Bem vindo ao jogo de SuperTrunfo!\nVamos cadastrar os dados das cidades, para isso siga as estapas descritas abaixo\n");
+carta = 1;
+
+printf("Cada estado será representado, por uma letra entre A e Hn/Digite uma letra entre A e H");
+scanf("%s",estado);
+
+printf("Cada cidade recebe um código vinculado ao seu estado.\n exemplo se voce informou estado A, e vai cadastrar a primeira cidade do estado A, o código da cidade será A01");
+printf("Digite o código da cidade a ser cadastrada"
+scanf("%s",codigo_da_carta);
 
   // Área para exibição dos dados da cidade
 
