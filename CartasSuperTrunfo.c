82627647247e11f2ecfe3ca47ee182c276b9a6c9
GROX <103297099+g4o4ox@users.noreply.github.com>
2026-02-24 22:48:25 -0300

    Implement city registration and data input
    Updated the main function to include city registration and data input for two cities. Added variables for storing city properties and improved output formatting.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,65 @@
 #include <stdio.h>
+// Projeto escrito e compilado em kernel Linux usando gcc
+// gcc main.c -o registrador_cidades
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+int main(){
+    
+    // Definindo variaveis
+    int habitantes = 0; // numero de habitantes
+    float pib; // valor do pib 
+    float area; // extensão da cidade em Km²
+    int pontos_turisticos; // pontos turisticos
+    char cidade[2]; // Introduzir o codigo da primeira cidade
+    char nome[50]; // nome da cidade
+    
+    int habitantes_2 = 0;
+    float pib_2;
+    float area_2;
+    int pontos_turisticos_2;
+    char cidade_2[2]; // Introduzir o codigo da segunda cidade
+    char nome_2[50];
 
-  // Área para exibição dos dados da cidade
+    // Entrada das informações da primeira cidade
+    printf("-----Registrar-Cidade------");
+    printf("\n-----Primeira-Cidade-------");
+    printf("\nDigite o nome da cidade: ");
+    scanf("%s", nome);
+    printf("Digite o numero de habitantes: ");
+    scanf("%d",&habitantes);
+    printf("\nDigite o valor do PIB: ");
+    scanf("%f", &pib);
+    printf("\nDigite a Area da cidade: ");
+    scanf("%f", &area);
+    printf("\nDigite a Quantidade de Pontos Turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\nEscolha uma letra de A-H: ");
+    scanf("%s", cidade);
+    
+    //Entrada das informações da segunda cidade
+    printf("\n------Registrar-cidade------ ");
+    printf("\n-----Segunda-Cidade----------");
+    printf("\nDigite o nome da segunda cidade: ");
+    scanf("%s", nome_2);
+    printf("\nDigite o numero de Habitantes: ");
+    scanf("%d", &habitantes_2);
+    printf("\nDigite o valor do PIB: ");
+    scanf("%f", &pib_2);
+    printf("\nDigite a Area da cidade: ");
+    scanf("%f", &area_2);
+    printf("\nDigite a Quantidade de Pontos Turisticos: ");
+    scanf("%d", &pontos_turisticos_2);
+    printf("\nEscolha uma letra de A-H: ");
+    scanf("%f",cidade_2);
+   
 
-return 0;
-} 
+    // Imprimindo todas as informações
+    printf("\n");
+    printf(" Cidade %s01 \n nome:%s \n habitantes:%d \n PIB:%f",cidade,nome,habitantes,pib);
+    printf("\nQtd.Pontos Turisticos:%d \n Area da cidade:%f",pontos_turisticos, area);
+    printf("\n");
+    printf("\nCidade %s02 \n nome:%s \n habitantes:%d \n PIB:%f",cidade_2,nome_2,habitantes_2,pib_2);
+    printf("\nQtd.Pontos Turisticos:%d \n Area da cidade:%f",pontos_turisticos_2, area_2);
+
+}
