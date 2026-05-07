1f5acdf07a2ed967640c4f54be56b0ba4d135dd0
alfredorobadel <alfredo.robadel@gmail.com>
2026-05-07 23:11:50 +0000

    n.c

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,49 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    /* 
-    Soma (+)
-    Subtração (-)
-    Multiplicação (*)
-    Divisão (/)
-    */
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b; // 'a' é convertido implicitamente para float
 
-    int numero1, numero2;
-    int soma, subtracao, multiplicacao, divisao;
+    printf("O resultado da soma é: %.2f\n", resultado);
 
-     printf("Entre com o numero 1: \n");
-     scanf("%d", &numero1);
-     printf("Entre com o numero 2: \n");
-     scanf("%d", &numero2);
-     // operção soma 
-    soma = numero1 + numero2;
-  
-     // operação subtração
-    subtracao = numero1 - numero2;
-
-     // operação multiplicação
-    multiplicacao = numero1 * numero2;
-
-     // operação divisão
-    divisao = numero1 / numero2;
-
-
-    printf("A Soma é: %d \n", soma);
-    printf("A Subtração é: %d \n", subtracao);
-    printf("A Multiplicação é: %d \n", multiplicacao);
-    printf("A Divisão é: %d \n", divisao);
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
-}
+    return 0;
+}   
+ 
