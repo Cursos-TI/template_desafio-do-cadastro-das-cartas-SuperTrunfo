21b80f4c109b5ccef9572619cf32bf5b84c6a29f
Wener Dev <wener20nascimento@gmail.com>
2026-05-22 19:00:21 -0300

    Variaveis inseridas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,21 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-//aaa
-
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char Carta[100];
+  char Estado[100];
+  char Codigo[100];
+  char Cidade[100];
+  int Populacao;
+  float Area;
+  float pib;
+  int pontos;
 
   // Área para entrada de dados
+  
 
   // Área para exibição dos dados da cidade
+  
 
 return 0;
 } 
